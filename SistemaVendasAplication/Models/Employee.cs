eecc326bc91464db788c1b839a4f409cf5184254
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-13 22:09:37 -0300

    Creating the models entity Employee

---
+++ SistemaVendasAplication/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace SistemaVendasAplication.Models
+{
+    public class Employee : Client
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "Minimo de 2 caracteris e maximo de 10")]
+        public string Login{ get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatório")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "A Lista de aturização é obrigatório")]
+        public List<string> Authorizations { get; set; }
+    }
+}
