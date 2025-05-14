c2e37c5a0345495e71c2b14ce80f07d6f7ac575d
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-13 22:11:46 -0300

    Creating the models entity Supplier

---
+++ SistemaVendasAplication/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendasAplication.Models
+{
+    public class Supplier : Client
+    {
+        [Required(ErrorMessage = "CNPJ é obrigatório")]
+        [StringLength(15, MinimumLength = 15, ErrorMessage = "Confira se todos os caracteris estão certo")]
+        public string CNPJ { get; set; }
+    }
+}
