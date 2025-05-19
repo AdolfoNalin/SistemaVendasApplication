08ce35228ec1bbd29577ec95c38bca207e27bcbc
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-19 20:57:45 -0300

    Craindo classes modelos ItemSale e ItemBudget

---
+++ SistemaVendasAplication/Models/ItemBudget.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SistemaVendasAplication.Models
+{
+    public class ItemBudget : ItemSale
+    {
+        [Required]
+        public Guid IdBudget { get; set; }
+
+        [Required]
+        public Guid Product { get; set; }
+
+        public ItemBudget()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ SistemaVendasAplication/Models/ItemSale.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace SistemaVendasAplication.Models
+{
+    public class ItemSale
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Venda é obrigatória")]
+        private Guid IdSale { get; set; }
+
+        [Required(ErrorMessage = "Produto é obrigatório")]
+        public Guid IdProduct { get; set; }
+
+        [Required(ErrorMessage = "Quantidade é obrigatório")]
+        public double Amount { get; set; }
+
+        [Required(ErrorMessage = "Subtotal é obrigatório")]
+        [Column(TypeName = "descimal(10,2)")]
+        public decimal Subtotal { get; set; }
+
+        public ItemSale()
+        {
+            Id = Guid.NewGuid();    
+        }
+
+        [JsonIgnore]
+        public Sale? Sale { get; set; }
+
+        [JsonIgnore]
+        public Product? Product { get; set; }
+    }
+}
