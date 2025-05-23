95220e1888c1683de4a6321586d3dbff229ba35b
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-22 21:27:14 -0300

    Configurando o contexto

--- SistemaVendasAplication/Program.cs
+++ SistemaVendasAplication/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaVendasAplication.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("ConnectionPostgres");
+builder.Services.AddDbContext<SysComAppDBContext>(x => x.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
