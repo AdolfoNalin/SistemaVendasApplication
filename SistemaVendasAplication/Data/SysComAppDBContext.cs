61fa76b28e9f85a9ace3a5864e6f438b34ec25fd
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-22 21:05:44 -0300

    Craindo a classe SysComAppDBContext

---
+++ SistemaVendasAplication/Data/SysComAppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVendasAplication.Models;
+
+namespace SistemaVendasAplication.Data
+{
+    public class SysComAppDBContext : DbContext
+    {
+        public SysComAppDBContext(DbContextOptions<SysComAppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Budget> Budget { get; set; }
+        public DbSet<Supplier> Supplier { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<ItemBudget> ItemBudget { get; set; }
+        public DbSet<ItemSale> ItemSale { get; set; }
+        public DbSet<ItemReturn> ItemReturn { get; set; }
+    }
+}
