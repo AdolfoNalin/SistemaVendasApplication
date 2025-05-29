43c7cbaa93042beb1f4d7f761c38ec0d70b0142d
AdolfoNalin <nalinadolfo33@gmail.com>
2025-05-29 15:46:43 -0300

    Atualizando as Migrations

---
+++ SistemaVendasAplication/Migrations/20250529004318_UpdateEntityEmployeeAndClient.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVendasAplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityEmployeeAndClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TelephoneNumber",
+                table: "Client",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RG",
+                table: "Client",
+                type: "character varying(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Client",
+                type: "character varying(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Client",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Client",
+                type: "character varying(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(12)",
+                oldMaxLength: 12);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TelephoneNumber",
+                table: "Client",
+                type: "character varying(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RG",
+                table: "Client",
+                type: "character varying(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Client",
+                type: "character varying(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Client",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Client",
+                type: "character varying(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(14)",
+                oldMaxLength: 14);
+        }
+    }
+}
