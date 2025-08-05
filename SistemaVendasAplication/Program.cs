1ed21ad3f0271f9bd16be46e34aa2a7994a9a66c
AdolfoNalin <nalinadolfo33@gmail.com>
2025-08-05 20:53:04 -0300

    Add the Authorize token

--- SistemaVendasAplication/Program.cs
+++ SistemaVendasAplication/Program.cs
@@ -1,30 +1,79 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SistemaVendasAplication.Data;
+using SistemaVendasAplication.Service;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-var connectionString = builder.Configuration.GetConnectionString("ConnectionPostgres");
-builder.Services.AddDbContext<SysComAppDBContext>(x => x.UseNpgsql(connectionString));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        //try
+        //{
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("ConnectionPostgres");
+            builder.Services.AddDbContext<SysComAppDBContext>(options => options.UseNpgsql(connectionString));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("key").Get<string>() ??
+                throw new ArgumentNullException("A chave Key não foi entrada!"));
+
+            builder.Services.AddAuthentication(
+                x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+            )
+            .AddJwtBearer(
+                x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                }
+            );
+
+            builder.Services.AddSingleton<TokenGenerator>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        /*}
+        catch (ArgumentNullException ane)
+        {
+            Console.WriteLine($"{ane.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+        }*/
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
